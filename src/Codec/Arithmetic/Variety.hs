560155bab9275fbe5d1f196665d6ac02ef5577ad
nbos <35620093+nbos@users.noreply.github.com>
2025-05-26 04:25:14 -0400

    Re-implemented correctly

--- src/Codec/Arithmetic/Variety.hs
+++ src/Codec/Arithmetic/Variety.hs
@@ -17,22 +17,31 @@ import Data.ByteString.Builder (toLazyByteString, word8, Builder)
 import Codec.Arithmetic.Variety.Bits (Precision(..))
 import qualified Codec.Arithmetic.Variety.Bits as Bits
 
--- | Convert [Bool] to ByteString
-bitsToBytestring :: [Bool] -> ByteString
+err :: String -> a
+err = error . ("Variety: " ++)
+
+-- | Convert String to ByteString
+bitsToBytestring :: String -> ByteString
 bitsToBytestring = toLazyByteString . go 0 0
   where
-    go :: Word8 -> Int -> [Bool] -> Builder
+    go :: Word8 -> Int -> String -> Builder
     go byte 8 bits = word8 byte <> go 0 0 bits
     go byte _ [] = word8 byte
     go byte i (b:bits) = go byte' (i + 1) bits
-      where byte' = byte .|. (fromIntegral (fromEnum b) `shiftL` i)
+      where
+        byte' = byte .|. (bit `shiftL` i)
+        bit = case b of '0' -> 0; '1' -> 1
+                        _ -> err $ "Non-binary char encountered: " ++ [b]
+-- TODO: compare against using readBin on 8-bit chunks
 
--- | Convert ByteString to [Bool]
-bytestringToBits :: ByteString -> [Bool]
+-- | Convert ByteString to String
+bytestringToBits :: ByteString -> String
 bytestringToBits = concatMap byteToBits . BS.unpack
   where
-    byteToBits :: Word8 -> [Bool]
-    byteToBits byte = map (testBit byte) [0..7]
+    byteToBits :: Word8 -> String
+    byteToBits byte = map (bool2char . testBit byte) [0..7]
+    bool2char b = if b then '1' else '0'
+-- TODO: compare against using writeBin
 
 -- | @'encode1' i n@ will encode a single value @i@ given its base @n@,
 -- or the number of possible values @i@ can take. Note that @i@ is an
@@ -46,11 +55,11 @@ encode1 = bitsToBytestring .: Bits.encode1
 -- number of values @i@ could take. Note that @i@ is an index and ranges
 -- from @[0..n-1]@, while @n@ is a length and is greater or equal to 1.
 encode :: Precision -> [(Integer, Integer)] -> ByteString
-encode  = bitsToBytestring .: Bits.encode
+encode = bitsToBytestring .: Bits.encode
 
 -- | Given a base, recover a single value from a serialization.
-decode1 :: Integer -> ByteString -> Integer
-decode1 n = Bits.decode1 n . bytestringToBits
+decode1 :: ByteString -> Integer
+decode1 = Bits.decode1 . bytestringToBits
 
 -- | Given the same precision policy and bases that were used to encode
 -- a sequence of values, recover the values from the serialization.
