86a18d5635622351fac948c9b3dba960962eec79
nbos <35620093+nbos@users.noreply.github.com>
2025-05-29 08:16:59 -0400

    rankMultisetPermutation

--- src/Codec/Arithmetic/Combinatorics.hs
+++ src/Codec/Arithmetic/Combinatorics.hs
@@ -1,10 +1,17 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Codec.Arithmetic.Combinatorics
   ( rankPermutation
   , unrankPermutation
+  , rankMultisetPermutation
+  , multinomial
   ) where
 
 import Control.Exception (assert)
+import Data.Maybe (fromJust)
 import qualified Data.Set as S
+import qualified Data.List as L
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
 import Math.Combinatorics.Exact.Factorial (factorial)
 
 import Codec.Arithmetic.Variety (Value(fromValue), mkValue, toBitVec, decode)
@@ -12,19 +19,57 @@ import Codec.Arithmetic.Variety (Value(fromValue), mkValue, toBitVec, decode)
 err :: String -> a
 err = error . ("Combinatorics: " ++)
 
+-- | Given a multiset permutation in the form of a list of indexes,
+-- return the number of times each index appears for @[0..maxIndex]@
+-- (fst) (the multiset) as well as the index of the given permutation in
+-- the lexicographic enumeration of all permutations of that multiset
+-- (fst . snd) with the total number of permutations in that enumeration
+-- (snd . snd) i.e. the multinomial coefficient with a composition equal
+-- to the multiset.
+rankMultisetPermutation :: forall a. Ord a => [a] ->
+                           ([(a,Int)], (Integer, Integer))
+rankMultisetPermutation msp = ( M.toList counts
+                              , (index, coef0) )
+  where
+    counts = L.foldl' (\m k -> M.insertWith (+) k 1 m) M.empty msp
+    total0 = sum counts
+    coef0 = factorial total0
+            `div` product (factorial <$> counts)
+    index = sum $ go (fromIntegral total0) coef0 counts msp
+
+    go :: Integer -> Integer -> Map a Int -> [a] -> [Integer]
+    go _ _ _ [] = []
+    go total coef m (a:as) = sum lowerSubCoefs :
+                             go total' coef'' m' as
+      where
+        (lt,eq,_) = M.splitLookup a m
+        total' = total - 1 -- decrement `total` by 1
+        coef' = coef `div` total -- rm `total` factor from num
+        lowerSubCoefs = (coef' *) . fromIntegral <$> lt
+        n = fromJust eq
+        n' = n - 1 -- decrement `a`'s count by 1
+        coef'' = coef' * fromIntegral n -- rm `n` factor from denom
+        m' = M.update (\_ -> if n' == 0 then Nothing else Just n')
+             a m
+
+multinomial :: [Int] -> Integer
+multinomial ns = factorial (sum ns)
+                 `div` product (factorial <$> ns)
+
 -- | Given a permutation of `n` elements, return its encoding/index
 -- (fst) in the lexicographic enumeration of all permutations of those
 -- elements as well as the total number of possible permutations (snd)
--- `n!`.
+-- `n!`. This is the same as ranking the multiset permutation where each
+-- element only appears once.
 rankPermutation :: Ord a => [a] -> (Integer, Integer)
-rankPermutation as | length as /= n0 = err' "Not a list of unique elements"
-                   | otherwise = fromValue val
+rankPermutation p | length p /= n0 = err' "Not a list of unique elements"
+                  | otherwise = fromValue val
   where
     err' = err . ("rankPermutation: " ++)
-    s0 = S.fromList as
+    s0 = S.fromList p
     n0 = S.size s0
     ns = fromIntegral <$> [n0,n0-1..1]
-    is = fromIntegral <$> go s0 as
+    is = fromIntegral <$> go s0 p
     val = assert (length is == length ns)
           mconcat $
           zipWith mkValue is ns
@@ -37,7 +82,8 @@ rankPermutation as | length as /= n0 = err' "Not a list of unique elements"
 
 -- | Given a list of `n` unique elements and an index `i < n!`, produce
 -- the permutation at index `i` of the lexicographic enumeration of all
--- permutations of those `n` elements.
+-- permutations of those `n` elements. This is the same as unranking the
+-- multiset permutation where each element only appears once.
 unrankPermutation :: Ord a => [a] -> Integer -> [a]
 unrankPermutation as i0 | length as /= n0 = err' "Not a list of unique elements"
                         | otherwise = go s0 is
