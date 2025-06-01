c1d09bbdb41ec76ef5a0a80f8b6204a617b0d013
nbos <35620093+nbos@users.noreply.github.com>
2025-06-01 06:49:03 -0400

    Documentation for Elias

--- src/Codec/Elias.hs
+++ src/Codec/Elias.hs
@@ -1,13 +1,31 @@
+-- | Elias codes are prefix codes for positive, non-zero integers with
+-- no prior assumption to their size.
 module Codec.Elias
     ( -- * Gamma coding
+
+      -- | An Elias gamma code consists of the binary expansion of an
+      -- integer, preceded by the unary encoding of the length of that
+      -- expansion as a string of zeros.
+
       encodeGamma
     , decodeGamma
 
     -- * Delta coding
+
+    -- | An Elias delta code is like an Elias gamma code except that the
+    -- length is itself coded in a gamma code instead of its unary
+    -- encoding.
+
     , encodeDelta
     , decodeDelta
 
     -- * Omega coding
+
+    -- | An Elias omega code is the result of recursively encoding the
+    -- length of binary expansions until a length of @1@ is
+    -- reached. Since binary expansions are written without any leading
+    -- zeros, a single @0@ bit marks the end of the code.
+
     , encodeOmega
     , decodeOmega
     ) where
@@ -16,24 +34,23 @@ import Data.Bifunctor (Bifunctor(first))
 import Codec.Arithmetic.Variety.BitVec (BitVec)
 import qualified Codec.Arithmetic.Variety.BitVec as BV
 
-err :: String -> a
-err = error . ("Elias: " ++)
+err :: a
+err = error "Elias: Number must be positive and non-zero"
 
--- | Encode a positive, non-zero, number in a Elias gamma code.
+-- | Encode a number in a Elias gamma code. Throws an error if the input
+-- is not positive and non-zero.
 encodeGamma :: Integer -> BitVec
 encodeGamma n | n > 0 = zeros <> bv
-              | otherwise =
-                  err $ "encodeGamma: "
-                  ++ "Number must be positive and non-zero"
+              | otherwise = err
   where
     len = BV.bitLen n
     zeros = BV.replicate (len-1) False
     bv = BV.bitVec len n
 
--- | Try to decode a Elias gamma code at the head of the given bit
--- vector. Upon success, returns the value and remainder of the bitvec,
--- with the encoding prefix removed. Returns @Nothing@ if the bit vector
--- doesn't contain enough bits to define a number.
+-- | Try to decode an Elias gamma code at the head of the given bit
+-- vector. If successful, returns the decoded value and the remainder of
+-- the `BitVec`, with the value code removed. Returns @Nothing@ if the
+-- bit vector doesn't contain enough bits to define a number.
 decodeGamma :: BitVec -> Maybe (Integer, BitVec)
 decodeGamma bv | BV.length valBits /= len = Nothing
                | otherwise = Just (val, bv'')
@@ -43,15 +60,19 @@ decodeGamma bv | BV.length valBits /= len = Nothing
     (valBits, bv'') = BV.splitAt len bv'
     val = BV.toInteger valBits
 
+-- | Encode a number in a Elias delta code. Throws an error if the input
+-- is not positive and non-zero.
 encodeDelta :: Integer -> BitVec
 encodeDelta n | n > 0 = encodeGamma (fromIntegral len) <> bv
-              | otherwise =
-                  err $ "encodeDelta: "
-                  ++ "Number must be positive and non-zero"
+              | otherwise = err
   where
     len = BV.bitLen n
     bv = BV.bitVec len n
 
+-- | Try to decode an Elias delta code at the head of the given bit
+-- vector. If successful, returns the decoded value and the remainder of
+-- the `BitVec`, with the value code removed. Returns @Nothing@ if the
+-- bit vector doesn't contain enough bits to define a number.
 decodeDelta :: BitVec -> Maybe (Integer, BitVec)
 decodeDelta bv = do
   (len, bv') <- first ((+1) . fromIntegral) <$> decodeGamma bv
@@ -59,11 +80,11 @@ decodeDelta bv = do
   if BV.length valBits /= len then Nothing
     else Just (BV.toInteger valBits, bv'')
 
+-- | Encode a number in a Elias omega code. Throws an error if the input
+-- is not positive and non-zero.
 encodeOmega :: Integer -> BitVec
 encodeOmega n0 | n0 > 0 = go n0 eom
-               | otherwise =
-                   err $ "encodeOmega: "
-                   ++ "Number must be positive and non-zero"
+               | otherwise = err
   where
     eom = BV.bitVec 1 0 -- "0"
 
@@ -73,6 +94,10 @@ encodeOmega n0 | n0 > 0 = go n0 eom
         bv = BV.fromInteger n
         len = fromIntegral $ BV.length bv
 
+-- | Try to decode an Elias omega code at the head of the given bit
+-- vector. If successful, returns the decoded value and the remainder of
+-- the `BitVec`, with the value code removed. Returns @Nothing@ if the
+-- bit vector doesn't contain enough bits to define a number.
 decodeOmega :: BitVec -> Maybe (Integer, BitVec)
 decodeOmega = go 1
   where
--- src/Codec/Elias/Natural.hs
+++ src/Codec/Elias/Natural.hs
@@ -1,13 +1,35 @@
+-- | Elias codes are prefix codes for positive, non-zero integers with
+-- no prior assumption to their size.
+--
+-- To allow for encoding zero, functions of this module add @1@ at
+-- encoding time and subtract @1@ at decoding time to support any
+-- natural number.
 module Codec.Elias.Natural
     ( -- * Gamma coding
+
+      -- | An Elias gamma code consists of the binary expansion of an
+      -- integer, preceded by the unary encoding of the length of that
+      -- expansion as a string of zeros.
+
       encodeGamma
     , decodeGamma
 
     -- * Delta coding
+
+    -- | An Elias delta code is like an Elias gamma code except that the
+    -- length is itself coded in a gamma code instead of its unary
+    -- encoding.
+
     , encodeDelta
     , decodeDelta
 
     -- * Omega coding
+
+    -- | An Elias omega code is the result of recursively encoding the
+    -- length of binary expansions until a length of @1@ is
+    -- reached. Since binary expansions are written without any leading
+    -- zeros, a single @0@ bit marks the end of the code.
+
     , encodeOmega
     , decodeOmega
     ) where
@@ -16,31 +38,44 @@ import Data.Bifunctor (Bifunctor(first))
 import Codec.Arithmetic.Variety.BitVec (BitVec)
 import qualified Codec.Elias as E
 
-err :: String -> a
-err = error . ("Elias.Natural: " ++)
+err :: a
+err = error "Elias.Natural: negative number"
 
--- | Encode a positive, non-zero, number in a Elias gamma code.
+-- | Encode a number in a Elias gamma code. Throws an error if the input
+-- is negative.
 encodeGamma :: Integer -> BitVec
 encodeGamma n | n >= 0 = E.encodeGamma (n+1)
-              | otherwise = err "encodeGamma: negative number"
+              | otherwise = err
 
--- | Try to decode a Elias gamma code at the head of the given bit
--- vector. Upon success, returns the value and remainder of the bitvec,
--- with the encoding prefix removed. Returns @Nothing@ if the bit vector
--- doesn't contain enough bits to define a number.
+-- | Try to decode an Elias gamma code at the head of the given bit
+-- vector. If successful, returns the decoded value and the remainder of
+-- the `BitVec`, with the value code removed. Returns @Nothing@ if the
+-- bit vector doesn't contain enough bits to define a number.
 decodeGamma :: BitVec -> Maybe (Integer, BitVec)
 decodeGamma = fmap (first (+(-1))) . E.decodeGamma
 
+-- | Encode a number in a Elias delta code. Throws an error if the input
+-- is negative.
 encodeDelta :: Integer -> BitVec
 encodeDelta n | n >= 0 = E.encodeDelta (n+1)
-              | otherwise = err "encodeDelta: negative number"
+              | otherwise = err
 
+-- | Try to decode an Elias delta code at the head of the given bit
+-- vector. If successful, returns the decoded value and the remainder of
+-- the `BitVec`, with the value code removed. Returns @Nothing@ if the
+-- bit vector doesn't contain enough bits to define a number.
 decodeDelta :: BitVec -> Maybe (Integer, BitVec)
 decodeDelta = fmap (first (+(-1))) . E.decodeDelta
 
+-- | Encode a number in a Elias omega code. Throws an error if the input
+-- is negative.
 encodeOmega :: Integer -> BitVec
 encodeOmega n | n >= 0 = E.encodeOmega n
-              | otherwise = err "encodeOmega: negative number"
+              | otherwise = err
 
+-- | Try to decode an Elias omega code at the head of the given bit
+-- vector. If successful, returns the decoded value and the remainder of
+-- the `BitVec`, with the value code removed. Returns @Nothing@ if the
+-- bit vector doesn't contain enough bits to define a number.
 decodeOmega :: BitVec -> Maybe (Integer, BitVec)
 decodeOmega = fmap (first (+(-1))) . E.decodeOmega
